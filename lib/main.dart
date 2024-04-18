303ac04122398980d2bbc685465b04543a3c3c09
sekhar1raja <86426237+sekhar1raja@users.noreply.github.com>
2024-04-17 21:37:09 -0400

    files created detail page

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,213 @@
 import 'package:flutter/material.dart';
+import 'detailpage.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: JobListPage(title: 'Job Details'), // Pass the title here
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
+class Job {
   final String title;
+  final String location;
+  final String description;
+  final String photo;
+  final double salary;
 
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Job({
+    required this.title,
+    required this.location,
+    required this.description,
+    required this.photo,
+    required this.salary,
+  });
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class JobListPage extends StatelessWidget {
+  final List<Job> jobs = [
+    Job(
+      title: 'Software Engineer',
+      location: 'San Francisco, CA',
+      description: 'Experienced software engineer needed for developing cutting-edge applications.',
+      photo: 'assets/job1.jpg',
+      salary: 100000,
+    ),
+    Job(
+      title: 'Graphic Designer',
+      location: 'New York, NY',
+      description: 'Creative graphic designer needed for designing captivating visual content.',
+      photo: 'assets/clay-elliot.jpg',
+      salary: 80000,
+    ),
+    Job(
+      title: 'Marketing Specialist',
+      location: 'Chicago, IL',
+      description: 'Skilled marketing specialist needed for planning and executing marketing campaigns.',
+      photo: 'assets/job3.jpg',
+      salary: 75000,
+    ),
+    Job(
+      title: 'Data Analyst',
+      location: 'Los Angeles, CA',
+      description: 'Analytical data analyst needed for interpreting complex datasets and providing insights.',
+      photo: 'assets/job4.jpg',
+      salary: 85000,
+    ),
+    Job(
+      title: 'UX/UI Designer',
+      location: 'Seattle, WA',
+      description: 'Talented UX/UI designer needed for creating intuitive and user-friendly interfaces.',
+      photo: 'assets/job5.jpg',
+      salary: 90000,
+    ),
+    Job(
+      title: 'Project Manager',
+      location: 'Austin, TX',
+      description: 'Organized project manager needed for overseeing project timelines and deliverables.',
+      photo: 'assets/job6.jpg',
+      salary: 95000,
+    ),
+  ];
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  final String title; // Add a title parameter
+
+  JobListPage({Key? key, required this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: Text(title), // Use the passed title here
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: ListView.builder(
+        itemCount: jobs.length,
+        itemBuilder: (context, index) {
+          final job = jobs[index];
+          return InkWell(
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => JobDetailPage(job: job),
+                ),
+              );
+            },
+            child: JobDetailCard(job: job),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class JobDetailCard extends StatefulWidget {
+  final Job job;
+
+  const JobDetailCard({Key? key, required this.job}) : super(key: key);
+
+  @override
+  _JobDetailCardState createState() => _JobDetailCardState();
+}
+
+class _JobDetailCardState extends State<JobDetailCard> {
+  bool _hovering = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(8.0),
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 200,
+              child: HoverWidget(
+                onHover: (value) {
+                  setState(() {
+                    _hovering = value;
+                  });
+                },
+                child: Transform.scale(
+                  scale: _hovering ? 1.1 : 1.0,
+                  child: Image.asset(
+                    widget.job.photo,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
             ),
+            const SizedBox(height: 16),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Title: ${widget.job.title}',
+              style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
             ),
+            const SizedBox(height: 8),
+            Text(
+              'Location: ${widget.job.location}',
+              style: const TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              'Salary: \$${widget.job.salary.toString()}',
+              style: const TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 8),
+            Text(
+              'Description: ${widget.job.description}',
+              style: const TextStyle(fontSize: 16),
+            ),
+            const SizedBox(height: 16),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class HoverWidget extends StatefulWidget {
+  final Widget child;
+  final ValueChanged<bool> onHover;
+
+  HoverWidget({required this.child, required this.onHover});
+
+  @override
+  _HoverWidgetState createState() => _HoverWidgetState();
+}
+
+class _HoverWidgetState extends State<HoverWidget> {
+  bool _isHovered = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return MouseRegion(
+      onEnter: (event) {
+        widget.onHover(true);
+        setState(() {
+          _isHovered = true;
+        });
+      },
+      onExit: (event) {
+        widget.onHover(false);
+        setState(() {
+          _isHovered = false;
+        });
+      },
+      child: widget.child,
     );
   }
 }
